b157d417f95bbb508892cf33e14695f77bfaeb71
David Dairo <david.adewoyin@baruchmail.cuny.edu>
2016-11-13 09:59:57 -0500

    Removed unneeded files and further tweaked excel functionality

--- app.js
+++ app.js
@@ -17,21 +17,19 @@ var style = wb.createStyle({
         color: '#000000',
         size: 12
     },
-    numberFormat: '$#,##0.00; ($#,##0.00); -'
+    numberFormat: '#,##0; (#,##0); -'
 });
 
 for (var i = 0; i < 50; i++) {
 
-	ws.cell(i+1,50).number(i+1).style(style);
-
-	wb.write('billboard.xlsx');
+	ws.cell(i+1,1).number(i+1).style(style);
 
 }
 
 ws.cell(1+1,2).string('My simple string').style(style);
 
-	// date format YYYY-MM-DD 
-
+	// Billboard methods
+	
 	// console.log(songs); //prints array of top 100 songs for week of August 27, 2016
     // console.log(songs[3]); //prints song with rank: 4 for week of August 27, 2016
     // console.log(songs[0].title); //prints title of top song for week of August 27, 2016
@@ -44,10 +42,12 @@ billboard(function(songs){
 
 	for (var i = 0; i < numberOfSongs; i++) {
 
-		ws.cell(i+1,50).number(i+1).style(style);
+		//ws.cell(i+1,1).string(songs[i].artist);
 
-		fs.appendFile('greetings.txt', i+1 + ") " + songs[i].artist + " - " + songs[i].title + "\n");
+		fs.appendFile('list.txt', i+1 + ") " + songs[i].artist + " - " + songs[i].title + "\n");
 
 	}
 
 });
+
+wb.write('billboard.xlsx');
